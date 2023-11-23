3fa932349ef00e52270fc8c36c502d349cb4e840
Sonny Piers <sonny@fastmail.net>
2023-11-23 13:44:48 +0100

    Fix compilation

--- code.rs
+++ code.rs
@@ -218,19 +218,17 @@ pub fn main() {
 
     {
         let add_button: gtk::Button = workbench::builder().object("add").unwrap();
-        let url_entry: gtk::Entry = workbench::builder().object("url").unwrap();
+        let url_entry: adw::EntryRow = workbench::builder().object("url").unwrap();
         let description_text_view: gtk::TextView =
             workbench::builder().object("description").unwrap();
 
         let sender = sender.clone();
 
         add_button.connect_clicked(move |_| {
-            let buffer = url_entry.buffer();
-            let url = buffer.text().to_string();
+            let url = url_entry.text().to_string();
             if url.is_empty() {
                 return;
             }
-            buffer.set_text("");
 
             let buffer = description_text_view.buffer();
             let (start, end) = buffer.bounds();
